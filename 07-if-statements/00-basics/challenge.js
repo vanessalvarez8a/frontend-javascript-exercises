6766c4ed59deea4167a78712050f37faff73ae8b
Vanessa <vanessalvarez8a@gmail.com>
2015-04-09 15:53:50 +0000

    if statements completed

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,43 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess) {
+  if (guess > fav) {
+    return "Too high";
+  }
+  else if (guess < fav) {
+    return "Too low";
+  }
+  else {
+    return "You got it!";
+  }
 
-module.exports.checkLock = undefined;
 
-module.exports.canIGet = undefined;
+};
+
+module.exports.checkLock = function(a,b,c,d) {
+  if ( (a == 3 || a == 5 || a == 7 ) && ( b === 2 ) && ( c >= 5 && c <= 100) && (d < 9 || d > 20) ) {
+    return "correct"
+  }
+  else {
+    return "incorrect"
+  }
+
+
+};
+  
+module.exports.canIGet = function(item, money) {
+  if (item == 'MacBook Air' && money >= 999) {
+    return true;
+  }
+  else if (item == 'MacBook Pro' && money >= 1299) {
+    return true;
+    }
+    else if (item == 'Mac Pro' && money >= 2499) {
+      return true;
+    }
+    else if (item == 'Apple Sticker' && money >= 1) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
+};
