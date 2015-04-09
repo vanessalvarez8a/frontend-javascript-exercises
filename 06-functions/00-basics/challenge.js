048035da3ceb630ba435b07395578781cf5a50fd
Vanessa <vanessalvarez8a@gmail.com>
2015-04-09 14:31:52 +0000

    Functions Completed

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,9 @@
-module.exports.helloWorld = undefined;
+module.exports.helloWorld = function() {
+  return "Hello World";
+};
 
-module.exports.multiply = undefined;
+
+
+module.exports.multiply = function(a,b) {
+  return a * b;
+};
