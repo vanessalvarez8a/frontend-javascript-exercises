4b5556816f3604e3398f9efcf82bdde36df48d77
Vanessa <vanessalvarez8a@gmail.com>
2015-04-11 19:05:06 +0000

    objects completed

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,39 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object[newProp]) {
+  }
+  else {
+    object[newProp] = newValue
+  }
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+ 
+    return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender; 
+};
 
-module.exports.canIGet = undefined;
+
+
+module.exports.canIGet = function(item, money) {
+  var allProducts = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1,
+  }   
+    return (allProducts[item] != undefined && money >= allProducts[item]);
+  }
+
+
+
+
+
